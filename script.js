e1fa46d890926c30bf9caf28f1b2ee1d8a4ff1fe
Al Hernandez <awessonn@gmail.com>
2023-02-20 14:39:38 -0800

    initial comments in .js file.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Assignment Code
 
-// I need a way to prompt the user with password length (8-128), lowercase, uppercase, numeric, and/or special characters.
+// (initial comment) I need a way to prompt the user with password length (8-128), lowercase, uppercase, numeric, and/or special characters.
 
 
-var generateBtn = document.querySelector("#generate"); //
+var generateBtn = document.querySelector("#generate"); // (initial comment)  This is the id for the red Generate Password button.
 
 
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword(); // (initial comment) generatePassword doesnt currently exist. 
+  var passwordText = document.querySelector("#password"); // (initial comment) #password is an id in the html file. This id displays the password onto the page where the id is located.
 
   passwordText.value = password; //
   
