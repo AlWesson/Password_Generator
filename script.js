06d675bb802456f3ad8c9cac149bcf908bbf2d99
Al Hernandez <awessonn@gmail.com>
2023-02-20 09:10:12 -0800

    criteria comments.

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+// I need a way to prompt the user with password length (8-128), lowercase, uppercase, numeric, and/or special characters.
+
+
+var generateBtn = document.querySelector("#generate"); //
+
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.value = password; //
+  
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); // 
